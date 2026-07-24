a3647474f7b4988c346e3f6d479353f9db45932a
abhi <abhinayasangepu6@gmail.com>
2026-07-24 23:14:50 +0530

    Connect frontend with Render backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://fundsroom-erp-ttnj.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
